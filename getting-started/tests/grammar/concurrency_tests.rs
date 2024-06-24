eb7d8a482590bebaed8b54ad93fe4e569a193d0d
kwseeker <xiaohuileee@gmail.com>
2024-06-24 21:10:25 +0800

    feat: getting-started, rust并发测试：使用Mutex、Channel两种方式实现账户并发扣款

--- getting-started/tests/grammar/concurrency_tests.rs
+++ getting-started/tests/grammar/concurrency_tests.rs
@@ -1,9 +1,25 @@
 //Rust并发编程
 //Rust 并发通信模型参考自Golang，也主张 “不要通过共享内存来通讯；而是通过通讯来共享内存。”
+//不过个人认为通信最终也是通过共享内存实现的，只不过实现流程上的区别，就好比响应式本质也是回调，但是却没有回调地狱的问题
+//通信方式：
+// mpsc::channel(); 这种方式类似于Java中被大量使用的生产者消费者模式：一些线程将数据发到队列，有一个线程从队列读取数据进行处理。
+//共享内存方式：
+// 互斥器
+//Send 与 Sync Trait （Rust 默认线程安全）
+// Send 标记 trait 表明实现了 Send 的类型值的所有权可以在线程间传送。几乎所有的 Rust 类型都是 Send 的。
+// Sync 标记 trait 表明一个实现了 Sync 的类型可以安全的在多个线程中拥有其值的引用。换一
+// 种方式来说，对于任意类型 T ，如果 &T （T 的不可变引用）是 Send 的话 T 就是 Sync 的，
+// 这意味着其引用就可以安全的发送到另一个线程。
+// 任何完全由 Send 的类型组成的类型也会自动被标记为 Send 。几乎所有基本类型都是 Send的，除了裸指针（raw pointer）
+//无惧并发编程：
+// 借助 Send 和 Sync，再配合所有权机制，带来的效果就是， Rust 能够在编译期就检查出数据竞争的隐患，而不需要等到运行时再排查。
 
 use std::alloc::System;
-use std::sync::mpsc;
+use std::rc::Rc;
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::mpsc::Sender;
 use std::thread;
+use std::thread::{JoinHandle, spawn};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 #[test]
@@ -108,4 +124,204 @@ fn thread_communication4() {
     for received in rx {
         println!("Got: {received}");
     }
+}
+
+//互斥器 Mutex
+//从官方注释看，lock() 是阻塞等待获取锁，如果别的获取锁的线程出现panic，则在获取互斥锁后，此调用将返回错误，且不支持重入
+//锁支持自动释放
+#[test]
+fn thread_mutex() {
+    //基于其他语言猜测，Rust Mutex 可能也是基于比较交换的方式实现的, 这里的传参是被保护的数据
+    let m = Mutex::new(5);
+    println!("m = {m:?}");
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+        // num = m.lock().unwrap();    //不支持重入
+    }   //自动解锁
+    println!("m = {m:?}");
+    {
+        let mut num = m.lock().unwrap();
+        println!("num = {num:?}");
+    }
+    println!("m = {m:?}");
+}
+
+#[test]
+fn thread_mutex2() {
+    let balance = 100;
+    //下面两个线程操作的余额和上面balance完全是不同的值
+    let m = Arc::new(Mutex::new(balance));  //balance值传递
+
+    let m1 = Arc::clone(&m);    //这里克隆实现多所有权而不是重新深克隆了一个值，不同线程是可以读取彼此修改后的最新值的
+    let h1 = thread::spawn(move || {
+        let mut balance = m1.lock().unwrap();
+        *balance += 10;
+        println!("thread1 exec: num = {balance:?}");
+        thread::sleep(Duration::from_millis(100));
+        println!("thread1 done");
+    });
+
+    let m2 = Arc::clone(&m);
+    let h2 = thread::spawn(move || {
+        let mut balance = m2.lock().unwrap();
+        *balance -= 20;
+        println!("thread2 exec: num = {balance:?}");
+        thread::sleep(Duration::from_millis(100));
+        println!("thread2 done");
+    });
+
+    h1.join().unwrap();
+    h2.join().unwrap();
+    println!("balance = {balance}");    //100
+}
+
+// 账户扣款，互斥锁方式
+#[test]
+fn thread_deduct_balance() {
+    let balance = Mutex::new(100);
+    let arc_balance = Arc::new(balance);
+    let mut thread_vec = vec![];
+    //开20个线程扣款，每个线程扣10元
+    for i in 0..20 {
+        let arc_balance = Arc::clone(&arc_balance);
+        let handle = thread::spawn(move || {
+            let mut balance = arc_balance.lock().unwrap();
+            //稍微延迟一下确保各个线程发生竞争
+            thread::sleep(Duration::from_millis(50));
+            if *balance >= 10 {
+                *balance -= 10;
+            } else {
+                println!("balance not enough!")
+            }
+        });
+        thread_vec.push(handle);
+    }
+
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+
+    let final_balance = arc_balance.lock().unwrap();
+    assert_eq!(0, *final_balance);
+    println!("final balance: {final_balance}")
+}
+
+// 账户扣款，通信方式
+#[derive(Debug)]
+struct DeductMessage {
+    tx: Sender<DeductResult>,
+    deduct: u32,
+}
+
+#[derive(Debug)]
+enum DeductCode {
+    SUCCESS {code: String},
+    FAILED {code: String, reason: String}
+}
+
+struct DeductResult {
+    code: DeductCode,
+    balance: Option<u32>,
+}
+
+#[test]
+fn thread_deduct_balance_by_channel() {
+    let mut balance = 100u32;
+    let (tx, rx) = mpsc::channel();
+    let mut thread_vec = vec![];
+
+    //开20个线程扣款，每个线程扣10元
+    for _ in 0..20 {
+        let tx = tx.clone();
+        let handle = thread::spawn(move || {
+            let (cb_tx, cb_rx) = mpsc::channel();
+            //线程发消息说要扣款，实际扣款在主线程中处理
+            tx.send(DeductMessage{tx: cb_tx, deduct:  10u32}).unwrap();
+            let deduct_result = cb_rx.recv().unwrap();
+            match deduct_result.code {
+                DeductCode::SUCCESS {..} => {
+                    println!("deduct succeed: balance={}", deduct_result.balance.unwrap());
+                }
+                DeductCode::FAILED {reason, .. } => {
+                    println!("deduct failed: reason={}", reason);
+                }
+            }
+
+        });
+        thread_vec.push(handle);
+    }
+
+    let handle = thread::spawn(move || {
+        //接收消息，执行真正的扣款
+        let mut counter = 0;
+        for _ in 0..20 {
+            let received = rx.recv().unwrap();
+            println!("received deduct: {}", received.deduct);
+            if balance >= received.deduct {
+                balance -= received.deduct;
+                println!("deduct: {}, {balance}", received.deduct);
+                let deduct_result = DeductResult{code: DeductCode::SUCCESS {code: String::from("0")}, balance: Some(balance)};
+                received.tx.send(deduct_result).unwrap();
+            } else {
+                println!("balance not enough!");
+                let deduct_result = DeductResult{code: DeductCode::FAILED {
+                    code: String::from("1"), reason: String::from("balance not enough!")}, balance: None};
+                received.tx.send(deduct_result).unwrap();
+            }
+            counter += 1;
+        }
+    });
+    thread_vec.push(handle);
+
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+}
+
+// 掷硬币实验
+#[test]
+fn thread_deadlock() {
+    // 所有实验总共掷硬币次数
+    let total_flips = Arc::new(Mutex::new(0));  //从0开始计数， Mutex实现对计数的并发安全保护，Arc则用于实现多所有权并在各线程间共享
+    // 实验完成线程数量
+    let completed = Arc::new(Mutex::new(0));
+    let runs = 8;
+    let target_flips = 10;
+
+    // 开8个线程做投掷硬币实验，每个实验要求连续出现10次正面即结束
+    for _ in 0..runs {
+        let total_flips = total_flips.clone();
+        let completed = completed.clone();
+        thread::spawn(move || {                         //因为 Mutex 没有实现 Copy 特征
+            flip_simulate(target_flips, total_flips);
+            let mut completed = completed.lock().unwrap();
+            *completed += 1;
+        });
+    }
+    loop {
+        let completed = completed.lock().unwrap();
+        if *completed == runs {
+            let total_flips = total_flips.lock().unwrap();
+            println!("Final average: {}", *total_flips / *completed);
+            break;
+        }
+    }
+}
+
+fn flip_simulate(target_flips: u64, total_flips: Arc<Mutex<u64>>) {
+    let mut continue_positive = 0;
+    let mut iter_counts = 0;
+    while continue_positive <= target_flips {
+        iter_counts += 1;
+        let pro_or_con = rand::random();
+        if pro_or_con {
+            continue_positive += 1;
+        } else {
+            continue_positive = 0;
+        }
+    }
+    println!("iter_counts: {}", iter_counts);
+    let mut total_flips = total_flips.lock().unwrap();
+    *total_flips += iter_counts;
 }
--- getting-started/tests/grammar/trait_tests.rs
+++ getting-started/tests/grammar/trait_tests.rs
@@ -1,5 +1,22 @@
 // Trait 定义共同行为
 // 定位和其他语言的接口比较类似， trait：意为特征。
+// 可以说 trait 是 Rust 的灵魂。 Rust 中所有的抽象，比如接口抽象、 OOP 范式抽象、函数式范式抽象等，
+// 均基于 trait 来完成。同时， trait 也保证了这些抽象几乎都是运行时零开销的。
+// trait 有如下 4 种用法：
+//  接口抽象。接口是对类型行为的统一约束。（impl <Trait> for <T>）
+//  泛型约束。泛型的行为被 trait 限定在更有限的范围内。
+//  抽象类型。在运行时作为一种间接的抽象类型去使用，动态地分发给具体的类型。
+//  标签 trait。对类型的约束，可以直接作为一种“标签”使用。
+// 孤儿规则：
+//  如果要实现某个 trait，那么该 trait 和要实现该 trait 的那个类型至少有一个要在当前 crate 中定义。
+// trait 继承：
+//  子 trait 可以继承父 trait 中定义或实现的方法。但是不支持传统面向对象的类型继承
+// trait 限定（Bound）
+// where 关键字：
+//  如果为泛型增加比较多的 trait 限定，代码可能会变得不太易读，比如下面这种写法：
+//  fn foo<T: A, K: B+C, R: D>(a: T, b: K, c: R){. . .}
+//  Rust 提供了 where 关键字，用来对这种情况进行重构：
+//  fn foo<T, K, R>(a: T, b: K, c: R) where T: A, K: B+C, R: D {. . .}
 
 // ------------------------------------------------------------------
 pub trait Fly {
@@ -63,12 +80,13 @@ impl Fly for Bird {
 #[test]
 fn test_trait() {
     // let human = Human{};
-    let human = Human{ 0: () };
+    let human = Human { 0: () };
     // let bird = Bird{};
-    let bird = Bird{ 0: () };
+    let bird = Bird { 0: () };
     human.eat();
     human.drink();
     human.work();
+    Human::do_something();
     bird.eat();
     bird.drink();
     bird.fly();
@@ -77,7 +95,7 @@ fn test_trait() {
 // ------------------------------------------------------------------
 // Trait 派生
 // 调试中常派生 Debug Trait
-#[derive(Debug)]    //相当于
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -92,4 +110,45 @@ fn test_derive() {
     println!("rect1 is {rect1:?}"); //在 {} 中加入 :? 指示符告诉 println! 我们想要使用叫做 Debug 的输出格式
     println!("rect1 is {rect1:#?}");
     // println!("rect1 is {rect1}"); //会报错
+}
+
+// ------------------------------------------------------------------
+// Trait 继承
+trait Page {
+    fn set_page(&self, p: i32) {
+        println!("Page Default: 1");
+    }
+}
+
+trait PerPage {
+    fn set_per_page(&self, num: i32) {
+        println!("Per Page Default: 10");
+    }
+}
+
+struct MyPaginate {
+    page: i32,
+}
+
+impl Page for MyPaginate {}
+
+impl PerPage for MyPaginate {}
+
+// Paginate 继承 Page PerPage 特征
+trait Paginate: Page + PerPage {
+    fn set_skip_page(&self, num: i32) {
+        println!("Skip Page : {:?}", num);
+    }
+}
+
+//通过泛型为所有同时拥有 Page 和 PerPage 行为的类型实现 Paginate
+//impl<T: A + B> C for T 即 为所有 T⊂(A∩ B)实现 Trait C
+impl<T: Page + PerPage> Paginate for T {}
+
+#[test]
+fn test_trait_inherit() {
+    let my_paginate = MyPaginate { page: 1 };
+    my_paginate.set_page(2);
+    my_paginate.set_per_page(100);
+    my_paginate.set_skip_page(3);
 }
